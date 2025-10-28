084292f2f895d0e7bf2e1e9cefbd7f912aba6911
kinanthiwibowo <kinanthiwibowo22>
2025-10-28 14:00:41 +0700

    update

--- resources/views/Kasir/nota_pembayaran.blade.php
+++ resources/views/Kasir/nota_pembayaran.blade.php
@@ -1,1 +1,9 @@
-ad
+<div class="v_nota mt-2">
+    <div class="card">
+        <div class="card-header">Nota Pembayaran</div>
+        <div class="card-body">
+
+        </div>
+        <div class="card-footer"><button class="btn btn-success"><i class="bi bi-cash-coin"></i> Bayar</button></div>
+    </div>
+</div>
